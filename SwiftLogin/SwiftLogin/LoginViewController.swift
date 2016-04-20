41b6980f6461c7d4b10df52a7498f42f9cfca3b5
Jonah Kuria <jonah.kuria@hotmail.com>
2016-04-20 05:13:25 +0200

    setup the application

---
+++ SwiftLogin/SwiftLogin/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  SwiftLogin
+//
+//  Created by Jonah Kuria on 2016/04/20.
+//  Copyright © 2016 kuria. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
