ac30dfa7e0138bc83861a2c8a777a783055a2d57
Jonah Kuria <jonah.kuria@hotmail.com>
2016-04-20 05:56:39 +0200

    implement the signIn action

--- SwiftLogin/SwiftLogin/LoginViewController.swift
+++ SwiftLogin/SwiftLogin/LoginViewController.swift
@@ -17,6 +17,25 @@ class LoginViewController: UIViewController {
 
 
   @IBAction func signIn(sender: AnyObject) {
+    loginActivityIndicator.hidden = false
+    loginActivityIndicator.startAnimating()
+
+    let username = usernameTextField.text
+    let password = passwordTextField.text
+
+    let currentUsername = "admin"
+    let currentPassword = "admin"
+
+    func login(username: String, password: String) {
+      if (username == currentUsername && password == currentPassword) {
+        performSegueWithIdentifier("loginSegue", sender: self)
+      }
+    }
+
+    if (username != "" && password != "") {
+      login(username!, password: password!)
+    }
+
   }
 
   override func viewDidLoad() {
