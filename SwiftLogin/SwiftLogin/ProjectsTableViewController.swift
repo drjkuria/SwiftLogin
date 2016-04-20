aef0359460486acb0fce320d8ed51965cfeb6067
Jonah Kuria <jonah.kuria@hotmail.com>
2016-04-20 06:08:20 +0200

    add projects table view controller

---
+++ SwiftLogin/SwiftLogin/ProjectsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProjectsTableViewController.swift
+//  SwiftLogin
+//
+//  Created by Jonah Kuria on 2016/04/20.
+//  Copyright © 2016 kuria. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsTableViewController: UITableViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
